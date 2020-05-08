e0e392dba9bb4046703e7f9419bfe5297932775c
aram <aramhd99@gmail.com>
2020-05-08 04:44:41 +0300

    added my picture

---
+++ src/MyPicture/Mypicture.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Picture from '../Images/aram.jpg';
+
+const myPicture = (props) => (
+    <img height={props.height}
+    src={Picture}
+    />
+)
+
+export default myPicture;
