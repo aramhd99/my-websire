3d6f51ade9b9ed03f3456e9ca34d0090fa0fb13d
aram <aramhd99@gmail.com>
2020-05-08 04:24:01 +0300

    typo error fixed

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,7 +6,7 @@ import Button from '../UI/Button/Button';
 const home = () => (
     <div className={classes.Home}>
         <h1>Hello everyone and, wellcome to my official website</h1>
-        <h3>My name is <strong>Aram Alhaddad</strong> and, I am a web Devoloper and, a muscan</h3>
+        <h3>My name is <strong>Aram Alhaddad</strong> and, I am a web Devoloper and, a musician</h3>
         <Button>Learn More</Button>
     </div>
 );
