6a5dc225faa6b8154aa05945ae0d7136e0d7f3eb
aram <aramhd99@gmail.com>
2020-07-15 01:39:36 +0300

    fixed

--- src/About/About.js
+++ src/About/About.js
@@ -5,9 +5,9 @@ import classes from './About.module.css';
 const about = () =>
     <div className={classes.About}>
         <h1>About Me</h1>
-        <p>My name is Aram Jareer Alhaddad. I Was born on the 25th of september 1999 in Jordan, and my goal is To gain ground on which I would build my experience as a web developer in a professional environment
+        <p>My name is Aram Jareer Alhaddad. I Was born on the 25th of september 1999 in Jordan, and my goal is to gain ground on which I would build my experience as a web developer in a professional environment
             while using the latest and greatest methods, and technologies available.</p>
-        <p>I am a Front-End Web Developer with  with ReactJs, React Router, Redux, React Hocks.</p>
+        <p>I am a Front-End Web Developer with ReactJs, React Router, Redux, React Hocks.</p>
         <p>I am a musician, I play piano since I was 10 years old.</p>
     </div>
 
