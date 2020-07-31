4138355082ca851a8bdabc6488550707922ce8e5
aram <aramhd99@gmail.com>
2020-07-31 15:56:20 +0300

    before material ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
